29f691073bff1c0c1965ff80720bb5149c5ea97c
Marco Brianza <marcobrianza@gmail.com>
2016-03-03 15:10:26 +0100

    testing sketches

---
+++ Arduino/test_WiFiMQTT/test_WiFiMQTT.ino
@@ -0,0 +1,96 @@
+// This example uses an nodeMCU Amica 1.0 ESP8266
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://shiftr.io/try.
+//
+// based on the example by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
+
+#include <ESP8266WiFi.h>
+#include <MQTTClient.h>
+
+const char *ssid = "mySSID";
+const char *pass = "myPASS";
+
+const char *broker = "broker.shiftr.io";
+const char *clientId = "arduinoColor";
+const char *username = "try";
+const char *password = "try";
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+#define MSG_BYTES 3
+byte color[MSG_BYTES];
+byte newColor[MSG_BYTES];
+
+void connect(); // <- predefine connect() for setup()
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+  client.begin(broker, net);
+
+  connect();
+}
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect(clientId, username, password)) {
+    Serial.print(".");
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/color");
+  // client.unsubscribe("/example");
+}
+
+void loop() {
+  client.loop();
+  delay(10); // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 2000) {
+    lastMillis = millis();
+    color[0] = 5;
+    color[1] = 6;
+    color[2] = 7;
+    client.publish("/color", (char*)color, sizeof(color));
+  }
+}
+
+void messageReceived(String topic, String payload, char * mBytes, unsigned int mLength) {
+
+
+  Serial.print("incoming: ");
+  Serial.print(topic);
+  Serial.print(" - ");
+  Serial.print(payload);
+  Serial.print(" - ");
+  Serial.print(payload.length());
+  Serial.print(" - ");
+
+  if( mLength == MSG_BYTES) {
+    memmove(newColor, mBytes, MSG_BYTES);
+
+    Serial.print(byte(newColor[0]));  Serial.print(",");
+    Serial.print(byte(newColor[1]));  Serial.print(",");
+    Serial.print(byte(newColor[2]));  Serial.print(",");
+
+  }
+  Serial.println();
+}
