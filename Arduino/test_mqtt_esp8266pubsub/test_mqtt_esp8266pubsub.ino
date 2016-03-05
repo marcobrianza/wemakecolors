8e8b8db8076d17e14ae557213794d0aa8997c902
Marco Brianza <marcobrianza@gmail.com>
2016-03-05 23:06:45 +0100

    used new MQTT lib https://github.com/knolleary/pubsubclient

---
+++ Arduino/test_mqtt_esp8266pubsub/test_mqtt_esp8266pubsub.ino
@@ -0,0 +1,141 @@
+/*
+  Basic ESP8266 MQTT example modified for wemakecolors
+
+  This sketch demonstrates the capabilities of the pubsub library in combination
+  with the ESP8266 board/library.
+
+  It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
+
+  To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+  http://pubsubclient.knolleary.net/api.html
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+const char* SSID = "...";
+const char* PASSWORD = "...";
+
+//const char* MQTT_SERVER = "test.mosquitto.org";
+const char* MQTT_SERVER = "net.marcobrianza.it";
+const char* MQTT_TOPIC =   "/WeMakeColors/color";
+const char* MQTT_ID = "ColorArduino";
+
+const byte MSG_LEN = 3;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+byte myColor[MSG_LEN];
+byte newColor[MSG_LEN];
+int value = 0;
+
+// this is needed in Arduino 1.6.7
+void  setup_wifi();
+void callback(char* topic, byte* payload, unsigned int length);
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(MQTT_SERVER, 1883);
+  client.setCallback(callback);
+
+  //test color = teal
+  myColor[0] = 1;
+  myColor[1] = 130;
+  myColor[2] = 131;
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+
+  WiFi.begin(SSID, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((byte)payload[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  if (length == MSG_LEN) {
+    newColor[0] = payload[0];
+    newColor[1] = payload[1];
+    newColor[2] = payload[2];
+  }
+
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(MQTT_ID)) {
+      Serial.println("connected");
+      // ... and resubscribe
+      client.subscribe(MQTT_TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+
+    Serial.print("Publish message: ");
+    for (int i = 0; i < MSG_LEN; i++) {
+      Serial.print((byte)myColor[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+    client.publish(MQTT_TOPIC, myColor, 3);
+  }
+}
--- Processing/testMQTT/testMQTT.pde
+++ Processing/testMQTT/testMQTT.pde
@@ -13,48 +13,51 @@ int MSG_SIZE=3;
 byte c[];
 
 color newC;
-color CC=color(255,2,3); 
+color CC=color(255, 2, 3); //test color=red
 
 MQTTClient client;
 
 String clientID= "processingColor";
 //String mqttURL="mqtt://try:try@broker.shiftr.io";
 //String mqttURL="mqtt://test.mosquitto.org";
-String mqttURL="mqtt://broker.hivemq.com";
+String mqttURL="mqtt://net.marcobrianza.it";
 
 
 void setup() {
 
   c=new byte[MSG_SIZE]; 
-  
+
   c[0]=byte(red(CC));
   c[1]=byte(green(CC));
   c[2]=byte(blue(CC));
 
   client = new MQTTClient(this);
   client.connect(mqttURL, clientID);
-  client.subscribe("/color");
+  client.subscribe("/WeMakeColors/color");
   // client.unsubscribe("/color");
-  
 }
 
 void draw() {
 }
 
 void mousePressed() {
-  client.publish("/color", c);
+  client.publish("/WeMakeColors/color", c);
+}
+
+void connectionLost() {
+  println("### connection lost ##");
 }
 
 void messageReceived(String topic, byte[] payload) {
   print("new message: " + topic + " - "+ payload.length+" - ");
-  if ( payload.length==3) {
-    int r,g,b;
-    r=payload[0]& 0xff;
-    g=payload[1]& 0xff;
-    b=payload[2]& 0xff;
-    
-    println(r,g,b);
-    newC=color(r,g,b);
-    background(newC);
+  int r, g, b;
+  if (payload.length==MSG_SIZE){
+  r=payload[0]& 0xff;
+  g=payload[1]& 0xff;
+  b=payload[2]& 0xff;
+
+  println(r, g, b);
+  newC=color(r, g, b);
+  background(newC);
   }
 }
