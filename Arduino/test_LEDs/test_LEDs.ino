c76d81e8373aea12e3718adab9390da3b3bd1c1e
Marco Brianza <marcobrianza@gmail.com>
2016-03-23 15:03:59 +0100

    improve test_leds

--- Arduino/test_LEDs/test_LEDs.ino
+++ Arduino/test_LEDs/test_LEDs.ino
@@ -4,23 +4,23 @@
 #define DATA_PIN D1
 #define GLOBAL_BRIGHTNESS 255
 
-#define NUM_COLORS 9
 
 CRGB leds[NUM_LEDS];
-CRGB colors[NUM_COLORS];
 
 void setup() {
   FastLED.setBrightness(GLOBAL_BRIGHTNESS);
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);
   Serial.begin(115200);
+  randomSeed(analogRead(A0));
 
 }
 
 int calibrate( int rawVal) {
   float gammaL = 2.5;
+  float inRange = 16;
   float outRangeL = 255;
 
-  float inRange = 255;
+
   float l;
   float lC;
 
@@ -32,20 +32,15 @@ int calibrate( int rawVal) {
 
 void loop() {
 
-  int r = calibrate(random(0, 220));
-  int g = calibrate(random(0, 255));
-  int b = calibrate(random(0, 255));
+  int r = calibrate(random(0, 15));
+  int g = calibrate(random(0, 15));
+  int b = calibrate(random(0, 15));
 
-  for (int i = 0; i < NUM_COLORS - 1; i++) {
-    colors[i] = colors[i + 1];
+  for (int i = 0; i < NUM_LEDS - 1; i++) {
+    leds[i] = leds[i + 1];
   }
-
-  colors[NUM_COLORS - 1] = CRGB(r, g, b);
-  for (int c = 0; c < NUM_COLORS; c++)
-  {
-    leds[c] = colors[c];
-  }
-
+  leds[NUM_LEDS - 1] = CRGB(r, g, b);
+  
   FastLED.show();
 
   int d = random(8000, 16000);
