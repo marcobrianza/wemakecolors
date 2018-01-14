8fbe3c8eebd941a86bdc62e6cf0073709c08f962
Marco Brianza <marcobrianza@gmail.com>
2018-01-14 17:47:33 +0100

    initialise all LEDs with a color, set WiFi OFF

--- Arduino/test_LEDs/test_LEDs.ino
+++ Arduino/test_LEDs/test_LEDs.ino
@@ -3,11 +3,13 @@
 //
 // test LEDs
 
+
+
 #define FASTLED_ESP8266_RAW_PIN_ORDER
 #include <FastLED.h>
 
 #if defined(ESP8266)
-#define LED_DATA_PIN 5 //D2 is GPIO5
+#define LED_DATA_PIN 5 //D1 is GPIO5
 //#define FASTLED_ALLOW_INTERRUPTS 0
 #endif
 
@@ -22,37 +24,41 @@
 
 CRGB leds[NUM_LEDS];
 
+#include <ESP8266WiFi.h>
+
 void setup() {
+  WiFi.mode(WIFI_OFF);
+
   Serial.begin(115200);
   delay(2000);
   Serial.println("\n test LEDs");
 
   FastLED.setBrightness(GLOBAL_BRIGHTNESS);
+
+
+
   FastLED.addLeds<WS2812B, LED_DATA_PIN, GRB>(leds, NUM_LEDS);
-  memset(leds, 0, NUM_LEDS * 3);
-  FastLED.show();
+  //memset(leds, 0, NUM_LEDS * 3);
+
 
   randomSeed(analogRead(A0));
+
+  for (int i = 0; i < NUM_LEDS - 1; i++) {
+    leds[i] = newRndColor();
+  }
+
+  FastLED.show();
 }
 
 
 void loop() {
-  int r = random(0, MAX_C);
-  int g = random(0, MAX_C);
-  int b = random(0, MAX_C);
 
   for (int i = 0; i < NUM_LEDS - 1; i++) {
     leds[i] = leds[i + 1];
   }
 
-  byte R = cal_lut(r);
-  byte G = cal_lut(g);
-  byte B = cal_lut(b);
-
-  leds[NUM_LEDS - 1] = CRGB(R, G, B);
-
+  leds[NUM_LEDS - 1] = newRndColor();
   FastLED.show();
-  Serial.print(R); Serial.print(" "); Serial.print(G); Serial.print(" "); Serial.println(G);
 
   delay(6000);
 }
@@ -63,6 +69,19 @@ byte cal_lut(byte c) {
   return lut[c];
 }
 
+CRGB newRndColor() {
+  int r = random(0, MAX_C);
+  int g = random(0, MAX_C);
+  int b = random(0, MAX_C);
+
+  byte R = cal_lut(r);
+  byte G = cal_lut(g);
+  byte B = cal_lut(b);
+
+  Serial.print(R); Serial.print(" "); Serial.print(G); Serial.print(" "); Serial.println(G);
+
+  return CRGB(R, G, B);
+}
 
 
 
